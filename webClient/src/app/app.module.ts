d14224b8f7cb28fadb43c406e11917a2da6cc372
Reet Chowdhary <rchowdhary@rocketsoftware.com>
2019-01-17 13:31:01 -0500

    fixed modal overflow for too many files
    Signed-off-by: Reet Chowdhary <rchowdhary@rocketsoftware.com>

--- webClient/src/app/app.module.ts
+++ webClient/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { ButtonModule } from 'carbon-components-angular/button/button.module';
 import { InputModule } from 'carbon-components-angular/input/input.module';
 import { ModalModule } from 'carbon-components-angular/modal/modal.module';
 import { DropdownModule } from 'carbon-components-angular/dropdown/dropdown.module';
+import { LoadingModule } from 'carbon-components-angular/loading/loading.module';
 
 import { HttpClientModule } from '@angular/common/http';
 import { UploaderService } from './services/Uploader.service'
@@ -46,7 +47,8 @@ import { UploaderPanelComponent } from './uploader-panel/uploader-panel.componen
     ButtonModule,
     InputModule,
     ModalModule,
-    DropdownModule
+    DropdownModule,
+    LoadingModule
   ],
   providers: [FTAWebsocketService, UploaderService],
   bootstrap: [AppComponent]
